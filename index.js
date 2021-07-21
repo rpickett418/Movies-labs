082c9b07005dae5bedaa7cebc1b0b0bbc81958ae
rpickett418 <86981398+rpickett418@users.noreply.github.com>
2021-07-21 13:46:57 -0600

    movies lab

---
+++ index.js
@@ -0,0 +1,43 @@
+let message = document.querySelector('#message')
+function addMovie(event){
+
+event.preventDefault()
+
+let inputField = document.querySelector('input')
+let movie = document.createElement('li')
+let movieTitle = document.createElement('span')
+movieTitle.textContent = inputField.value
+
+
+movieTitle.addEventListener('click', crossOffMovie)
+
+movie.appendChild(movieTitle)
+
+let deleteBtn = document.createElement("button")
+deleteBtn.textContent = "X"
+deleteBtn.addEventListener('click', deleteMovie)
+movie.appendChild(deleteBtn)
+
+
+
+document.querySelector('ul').appendChild(movie) 
+inputField.value = ""
+
+}
+
+function deleteMovie(event){
+event.target.parentNode.remove()
+message.textContent = "Movie Deleted"
+
+}
+
+function crossOffMovie(event){
+event.target.classList.toggle('checked')
+if(event.target.classList.contains('checked') === true){
+    message.textContent = "Movied Watched"
+} else {
+    message.textContent = "Movie Added"
+}
+}
+
+document.querySelector('form').addEventListener("submit", addMovie)
